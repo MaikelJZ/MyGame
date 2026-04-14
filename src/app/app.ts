7d5af5d90f4a10d7a5b9222d4937266e201b1665
Maikel <maikeljulianz@gmail.com>
2026-04-13 22:49:38 -0300

    API.v1.1
    Criado todo o funcionamento e rota de API ,API funcionando, alterações no HTML da pagina inicial,

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit,signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameService } from './service/game.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('MyGame');
+
+  constructor(private gameService: GameService) {}
+
+  ngOnInit() {
+    this.gameService.getGames().subscribe(console.log);
+  }
 }
---
+++ src/app/service/game.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  private apiUrl = 'https://api.rawg.io/api';
+  private apiKey = 'CHAVE API';//Chave API
+
+  constructor(private http: HttpClient) {}
+
+  getGames() {
+    return this.http.get(`${this.apiUrl}/games?key=${this.apiKey}`);
+  }
+}
