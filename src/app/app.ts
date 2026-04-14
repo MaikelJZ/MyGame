fdc9b53af71ea95e2af1bc48319513a5554ac87b
Maikel <maikeljulianz@gmail.com>
2026-04-13 23:07:58 -0300

    API.v.1.2
    Criado a parte de mostrar 10 jogos aleatórios na pagina inicial , e função para recarregar com 10 jogos diferentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit,signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GameService } from './service/game.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('MyGame');
 
   constructor(private gameService: GameService) {}
 
+  games: any[] = [];
+  loading = false;
+
+  loadGames() {
+    this.loading = true;
+
+    this.gameService.getGames().subscribe((data: any) => {
+      const results = data.results;
+
+      const shuffled = results.sort(() => 0.5 - Math.random());
+
+      this.games = shuffled.slice(0, 10);
+
+      this.loading = false;
+    });
+  }
+
   ngOnInit() {
-    this.gameService.getGames().subscribe(console.log);
+    this.loadGames(); // 👈 chama aqui
   }
 }
